9d16f51dabbc7cb8fdfe41d0431181224f3053ff
Nathalie Pettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2024-01-22 22:37:48 +0100

    Add files via upload

---
+++ Calculator-WindowsForm/Calculator-WindowsForm/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_WindowsForm
+{
+    public partial class Form1 : Form
+    {
+
+        private string currentCalculation = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // For each function you make, you can choose that one in "Click event" under properties in your form-design
+        // This way the "drawing" will talk to the functions to know what to do
+        private void button_Click(object sender, EventArgs e)
+        {
+            // This adds the number or operator to the string calculation
+            currentCalculation += (sender as Button).Text;
+
+            // Display the current calculation back to the user
+            textBoxOutput.Text = currentCalculation;
+        }
+
+        private void button_Equals_Click(object sender, EventArgs e)
+        {
+            string formattedCalculation = currentCalculation.ToString().Replace("X", "*").ToString().Replace("&divide;", "/");
+
+            try
+            {
+                textBoxOutput.Text = new DataTable().Compute(formattedCalculation, null).ToString();
+                currentCalculation = textBoxOutput.Text;
+            }
+            catch (Exception ex)
+            {
+                textBoxOutput.Text = "0";
+                currentCalculation = "";
+            }
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            // Reset the calculation and empty the textbox
+            textBoxOutput.Text = "0";
+            currentCalculation = "";
+        }
+
+        private void button_ClearEntry_Click(object sender, EventArgs e)
+        {
+            // If the calculation is not empty, remove the last number/operator entered
+            if (currentCalculation.Length > 0)
+            {
+                currentCalculation = currentCalculation.Remove(currentCalculation.Length - 1, 1);
+            }
+
+            // Re-display the calculation onto the screen
+            textBoxOutput.Text = currentCalculation;
+        }
+
+    }
+}
